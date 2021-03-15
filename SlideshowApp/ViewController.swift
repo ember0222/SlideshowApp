17fd40df270e459d735fb89d2b60fd89a5f6e1fe
Ember <suzuki.n32@ma.tnc.ne.jp>
2021-03-16 01:16:37 +0900

    SlideshowAppのラベルを追加した。

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -9,11 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    let 画像の名前 = ["img1", "img2", "img3"]
+    var 画像番号 = 1
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    
+    
+    @IBAction func 次へボタンした時(_ sender: Any) {
+        
+        if 画像番号 == 1{
+            ImageView.image = UIImage(named:画像の名前[1])
+            画像番号 = 2
+            
+        } else if 画像番号 == 2 {
+        ImageView.image = UIImage(named:画像の名前[2])
+        画像番号 = 3
+        
+        } else {
+            ImageView.image = UIImage(named:画像の名前[0])
+            画像番号 = 1
+        
+    }
+    
 }
 
+}
